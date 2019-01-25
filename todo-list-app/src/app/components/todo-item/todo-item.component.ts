b1cdc8154f322ae35b7338a668d2b320842ea66d
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-25 10:02:14 +0200

    http and emiting delete event to parent

--- todo-list-app/src/app/components/todo-item/todo-item.component.ts
+++ todo-list-app/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { TodoService } from "./../../services/todo.service";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 import { Todo } from "src/app/models/Todo";
 
 @Component({
@@ -8,8 +9,9 @@ import { Todo } from "src/app/models/Todo";
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {}
 
@@ -23,9 +25,10 @@ export class TodoItemComponent implements OnInit {
 
   onToggle(todo) {
     todo.completed = !todo.completed;
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
-    console.log("delete");
+    this.deleteTodo.emit(todo);
   }
 }
