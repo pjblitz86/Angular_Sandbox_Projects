a35bb71481ee5aa5f814f70cfc4944c634ab7e7d
Paulius Juknis <paulius.juknis@gmail.com>
2019-04-23 10:44:22 +0300

    interpolation from hero model

--- app-heroes/src/app/heroes/heroes.component.ts
+++ app-heroes/src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { Hero } from "./../hero";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -6,8 +7,10 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  hero = "Windstorm";
-
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
   constructor() {}
 
   ngOnInit() {}
