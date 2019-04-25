aa13a5a7a62ebc19db746a672f18e509332445e6
Paulius Juknis <paulius.juknis@gmail.com>
2019-04-25 11:16:54 +0300

    add navbar with angular routing to list-employee and create employee components

--- ng-crud/src/app/app-routing.module.ts
+++ ng-crud/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from "./employees/create-employee.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListEmployeesComponent } from "./employees/list-employees.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "list", component: ListEmployeesComponent },
+  { path: "create", component: CreateEmployeeComponent },
+  { path: "", redirectTo: "/list", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ng-crud/src/app/app.module.ts
+++ ng-crud/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ListEmployeesComponent } from "./employees/list-employees.component";
+import { CreateEmployeeComponent } from "./employees/create-employee.component";
+import { NavComponent } from './layout/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ListEmployeesComponent],
+  declarations: [AppComponent, ListEmployeesComponent, CreateEmployeeComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
