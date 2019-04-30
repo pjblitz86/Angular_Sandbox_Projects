637e7feb9aff50a274eabbcf0a5956aaa0a64135
Paulius Juknis <paulius.juknis@gmail.com>
2019-04-30 16:22:53 +0300

    start create employee reactive form

--- ng6-tutorial/src/app/app.module.ts
+++ ng6-tutorial/src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './employee/create-employee.component';
-import { ListEmployeeComponent } from './employee/list-employee.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CreateEmployeeComponent } from "./employee/create-employee.component";
+import { ListEmployeeComponent } from "./employee/list-employee.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateEmployeeComponent,
-    ListEmployeeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CreateEmployeeComponent, ListEmployeeComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- ng6-tutorial/src/app/employee/create-employee.component.ts
+++ ng6-tutorial/src/app/employee/create-employee.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"]
 })
 export class CreateEmployeeComponent implements OnInit {
+  employeeForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.employeeForm = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl()
+    });
   }
-
 }
