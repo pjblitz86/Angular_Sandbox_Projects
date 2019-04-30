25def5878da5d52a4aa6c6101e2d93ca678c8d82
Paulius Juknis <paulius.juknis@gmail.com>
2019-04-30 11:25:07 +0300

    add createEmployee and ListEmployee components and include them in routing

--- ng6-tutorial/src/app/app-routing.module.ts
+++ ng6-tutorial/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from "./employee/create-employee.component";
+import { ListEmployeeComponent } from "./employee/list-employee.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "list", component: ListEmployeeComponent },
+  { path: "create", component: CreateEmployeeComponent },
+  { path: "", redirectTo: "/list", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
