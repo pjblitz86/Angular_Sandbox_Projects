3158e73d2e9dcdd63fd97e9752f153b523e10ec1
Paulius Juknis <paulius.juknis@gmail.com>
2019-05-08 13:43:22 +0300

    implement route resolve service

---
+++ ng-crud/src/app/employees/employee-list-resolver.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+import { Employee } from "../models/employee.model";
+import { Observable } from "rxjs";
+import { EmployeesService } from "./employees.service";
+
+@Injectable()
+export class EmployeeListResolverService implements Resolve<Employee[]> {
+  constructor(private _employeeService: EmployeesService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Employee[]> {
+    return this._employeeService.getEmployees();
+  }
+}
